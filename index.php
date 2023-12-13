813f435b65a3710f4ff401df94d529cf41a692be
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2023-12-13 15:51:10 +0100

    Add: form

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Iscriviti alla newsletter</h1>
+
+    <form action="" method="post">
+        <label for="email">Indirizzo Email:</label>
+        <input type="email" id="email" name="email" required>        
+        <input type="submit" value="Invia">        
+    </form>    
 </body>
 </html>
